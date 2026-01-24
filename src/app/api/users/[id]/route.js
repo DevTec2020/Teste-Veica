26213af29eedac1ae3255f80a5800eae8d42f2be
DevTec2020 <leonardo.silva.inf@gmail.com>
2026-01-24 16:25:00 -0300

    fix: corrigindo leitura de dados no PUT e DELETE

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -4,7 +4,7 @@ import { userRepo } from "@/lib/db";
 // PUT para atualizar usuário
 export async function PUT(request, { params }) {
   try {
-    const { id } = params;
+    const { id } = await params;
     const body = await request.json();
 
     const updatedUser = userRepo.update(id, body);
@@ -23,7 +23,7 @@ export async function PUT(request, { params }) {
 // DELETE Removendo o usuário do id
 export async function DELETE(request, { params }) {
   try {
-    const { id } = params;
+    const { id } = await params;
     userRepo.delete(id);
     
     return NextResponse.json({ message: "Usuário removido com sucesso" });
