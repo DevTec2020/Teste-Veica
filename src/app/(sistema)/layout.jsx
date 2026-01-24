fbc33392200fae6150a6093f94ad25f0d64a6fb5
DevTec2020 <leonardo.silva.inf@gmail.com>
2026-01-24 20:55:34 -0300

    Feat: adiocnando novo sidebar

--- src/app/(sistema)/layout.jsx
+++ src/app/(sistema)/layout.jsx
@@ -6,7 +6,7 @@ export default function DashboardLayout({ children }) {
     <div className="h-screen w-full bg-gray-100 flex overflow-hidden">
       <Sidebar />
       <div className="flex-1 flex flex-col min-w-0 h-full relative">
-        <main className="flex-1 overflow-y-auto bg-gray-100 w-full pt-16 md:pt-0">
+        <main className="flex-1 md:pt-0 overflow-y-auto h-screen bg-gray-100 w-full relative">
           {children}
         </main>
 
